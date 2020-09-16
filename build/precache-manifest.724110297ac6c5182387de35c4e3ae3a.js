e206aabda76dad5b250f05ca67bc0c037ff16e27
Fefe Lemaire <fede.lemaire@gmail.com>
2020-09-16 12:23:23 -0300

    buscador de gif

---
+++ build/precache-manifest.724110297ac6c5182387de35c4e3ae3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5537e506bd5172e9aca1ed1577694423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bf2c6fcf73e39cbe13b",
+    "url": "/static/css/main.7a0e6566.chunk.css"
+  },
+  {
+    "revision": "0c9b165fa602bcfd411b",
+    "url": "/static/js/2.7baf5ccf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7baf5ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf2c6fcf73e39cbe13b",
+    "url": "/static/js/main.ff554454.chunk.js"
+  },
+  {
+    "revision": "aaae2fe2200a2e500a17",
+    "url": "/static/js/runtime-main.9e32fd90.js"
+  }
+]);
---
+++ src/Components/SearchForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function SearchForm({ performSearch }) {
+
+  const [searchText, setSearchText] = useState('');
+
+  const onSearchChange = (e) => {
+    // Update state 
+    setSearchText(e.target.value)
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // pass the search text back to the App component
+    performSearch(searchText);
+    e.currentTarget.reset();
+  }
+
+  return (
+    <form className="search-form" onSubmit={handleSubmit}>
+      <label className="is-hidden" htmlFor="search">Search</label>
+      <input type="search"
+        onChange={onSearchChange}
+        name="search"
+        placeholder="Search..."
+      />
+      <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
+    </form>
+  );
+}
+
+export default SearchForm;
